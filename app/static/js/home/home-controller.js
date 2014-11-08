ae102e0e9e0f806b79e30d2c45b51fb8d761d39c
Sunny K Gupta <sunnylautner4@gmail.com>
2014-11-09 00:04:03 +0530

    Api-endPoint added : Data input and manipulation tested
    Nltk Library imported for further processing task
    
    API documentation

--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -1,8 +1,22 @@
 angular.module('data-tag')
-  .controller('HomeController', ['$scope', function ($scope) {
+  .controller('HomeController', ['$scope','$http', function ($scope, $http) {
 
-  	$scope.postData = function(data){
-  		alert(data);
+    $scope.resultData = {};
+  	$scope.postData = function(text){
+        // API target
+        var url = 'http://localhost:5000/api/tagit/v1.0/';
+        // Object for data
+        textData = {};
+        textData.text = text;
+        $http.post(url, textData).success(function(data, status){
+            console.log("Text Sent!");
+            console.log(data.text);
+            // Returned data from API end
+            $scope.resultData = data;
+        }).error(function(data, status){
+            console.log("Failure");
+        })
+
   	}
 
   }]);
