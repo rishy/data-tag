a8cc4b42cf9de2a84016558a709c3ed7acc7b2e1
Akshay Pratap Singh <pratapakshay0@gmail.com>
2014-12-13 17:47:06 +0530

    fix(worker) : add support of status in worker

--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -13,9 +13,13 @@ angular.module('data-tag')
 
     $scope.pages = {};
     $scope.showResult = false;
+
+    $scope.result = {};
+
   	$scope.postData = function(){
         // API target
-        var url = 'http://127.0.0.1:5000/api/tagit/v1.0/';
+        var url1 = 'http://127.0.0.1:5000/api/tagit/v1.0/';
+        var url2 = 'http://127.0.0.1:5000/api/tagit/v1.0/result/';
 
         if ($scope.input.link == undefined || $scope.input.link == ""){
             $scope.input.type = "text";
@@ -25,12 +29,35 @@ angular.module('data-tag')
 
         console.log($scope.input);
 
-        $http.post(url, $scope.input).success(function(data, status){
+        $http.post(url1, $scope.input).success(function(data, status){
             console.log("Text Sent!");
             console.log(data);
             // Returned data from API end
             $scope.showResult = true;
             $scope.pages = data;
+            $scope.result = data;
+            //  here result object has three properties
+            //  result.id => job key
+            //  result.all_nouns => all nouns extracted from text
+            //  result.status => status of job(pending, started, finished)
+
+        }).error(function(data, status){
+            //console.log(data);
+            console.log("Failure");
+        })
+
+        // add settimeout() function to send the http resquest for url2
+        // untill you get the result.status=finished
+        $http.post(url2, $scope.input).success(function($scope.result, status){
+            console.log("Text Sent!");
+            console.log(data);
+            // Returned data from API end
+            $scope.showResult = true;
+            $scope.result.pages = data;
+            //  here result object has three properties
+            //  result.id => job key
+            //  result.status => status of job(pending, started, finished)
+            //  result.pages => a list of result pages
         }).error(function(data, status){
             //console.log(data);
             console.log("Failure");
